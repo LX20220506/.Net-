19532d668094c72de78478e39ff3fd4d6b419177
LiuXiang <2748817041@qq.com>
2022-05-19 09:13:20 +0800

    完善RabbitMQ框架搭建

--- RabbitMQ/RabbitMQ高级特性/Demo.RabbitMq/Demo.RabbitMq.Base.RabbitMq/Consumer/RabbitChannelManager.cs
+++ RabbitMQ/RabbitMQ高级特性/Demo.RabbitMq/Demo.RabbitMq.Base.RabbitMq/Consumer/RabbitChannelManager.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Demo.RabbitMq.Base.RabbitMq.Config;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
